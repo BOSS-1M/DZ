9e8793f06152fe6c21244f2692227786a4c5fbc4
👑 𝐌𝐑 𝐑𝐈𝐂𝐎 👑 <max2005alg@gmail.com>
2025-07-03 15:36:42 +0100

    Delete run_N12_DECRYPTED_FINAL.py

--- run_N12_DECRYPTED_FINAL.py
+++
@@ -1,9 +0,0 @@
-
-# ملف تشغيل للوحدة المشفرة N12_DECRYPTED_FINAL.cpython-312.so
-import N12_DECRYPTED_FINAL
-
-# تأكد أن الدالة الرئيسية موجودة داخل الملف الأصلي
-if hasattr(N12_DECRYPTED_FINAL, 'main'):
-    N12_DECRYPTED_FINAL.main()
-else:
-    print("تم استيراد N12_DECRYPTED_FINAL بنجاح، ولكن لا توجد دالة main للتشغيل.")

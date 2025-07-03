f88887dab20adc4522f2154c85f7637945e6f07d
👑 𝐌𝐑 𝐑𝐈𝐂𝐎 👑 <max2005alg@gmail.com>
2025-07-03 15:36:08 +0100

    Delete MAYA.py

--- MAYA.py
+++
@@ -1,9 +0,0 @@
-
-# ملف تشغيل للوحدة المشفرة MAYA.cpython-312.so
-import MAYA
-
-# تأكد أن الدالة الرئيسية موجودة داخل الملف الأصلي
-if hasattr(MAYA, 'main'):
-    MAYA.main()
-else:
-    print("تم استيراد MAYA بنجاح، ولكن لا توجد دالة main للتشغيل.")
